4bd0f3c7dd328d05251e8dd09fda7936496b256c
Sunshine-wzy <sunshinewzy@qq.com>
2023-11-30 22:22:26 +0800

    配置环境

--- src/client/java/io/github/sunshinewzy/serverconductor/fabric/ServerConductorClient.java
+++ src/client/java/io/github/sunshinewzy/serverconductor/fabric/ServerConductorClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package io.github.sunshinewzy.serverconductor.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ServerConductorClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
